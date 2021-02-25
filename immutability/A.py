a9b4b1650adcd62fc7cc7b95a4414c12a9912896
amitpp <amitpp23@gmail.com>
2021-02-26 01:51:17 +0530

    This is Pandey

--- immutability/A.py
+++ immutability/A.py
@@ -1,4 +1,7 @@
-class A:
-    def __init__(self, name):
-        self.name = name
+class A: 
+    def __init__(self, name):    
+        self.__dict__['name'] = name 
+    def __setattr__(self, name, value):
+        if name == 'name':
+            raise ValueError('Not Allowed')
 
